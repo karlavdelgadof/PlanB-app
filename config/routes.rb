5590006b6d8f943ae3a3ba79ab9dc635ca7776dc
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-23 16:36:19 -0500

    Update schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :categories
   resources :procedures
   devise_for :users
+  root 'categories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
 end
