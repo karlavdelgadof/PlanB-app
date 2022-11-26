8358758ac5f9a73d0b09c6fbdfffa5ef628f9464
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-25 19:00:24 -0500

    Make icon an upload image for the user

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :procedures
+  has_one_attached :icon
 
   validates :name, :icon, presence: true
   validates :name, uniqueness: true
+
+  def icon_resize
+    icon.variant(resize_to_limit: [32, 32]).processed
+  end
 end
