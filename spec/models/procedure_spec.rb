ec5524d888d66f0a7c7e0b7ae8b8e15b0ab97d92
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-03 19:03:34 -0500

    Create unit and integration tests for categories

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Procedure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Aron Helu', email: 'aronhelu@gmail.com', password: '1234567890', password_confirmation: '1234567890')
+    @category = Category.create(name: 'category 2', user_id: @user.id)
+    @transaction = Procedure.create(name: 'transaction 1', amount: 22.4, user_id: @user.id )
+  end
+
+  it 'Inventoryfood should have valid attributes' do
+    expect(@transaction.name).to eq('transaction 1')
+  end
+
+  it 'Inventoryfood should have valid attributes' do
+    expect(@transaction.amount).to eq(22.4)
+  end
+
+  it 'user for inventory food should be valid' do
+    expect(@user).to be_valid
+  end
 end
