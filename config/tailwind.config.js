f9d2ecfae11e5716d29a6376b6fa9aeb9abf0ce9
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-03 22:44:55 -0500

    Remove flowbite modules

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
