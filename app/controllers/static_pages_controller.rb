d27bfcb4e50cdf0c4b77fd419ba6b4cde2d4038d
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-05 10:54:42 -0500

    Implement fixes and changes required

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,9 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def splash_page
-    respond_to do |format|
-      if user_signed_in?
-        format.html { redirect_to categories_url }
-      else
-        format.html { render :splash_page, status: :unprocessable_entity }
+    if user_signed_in?
+      respond_to do |format|
+          format.html { redirect_to categories_url }
       end
     end
   end
