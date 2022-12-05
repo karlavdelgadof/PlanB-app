39b29dcf34a19db6e83934c046314b84bc13b10a
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-05 10:56:33 -0500

    Fix rubocop offenses

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,10 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def splash_page
-    if user_signed_in?
-      respond_to do |format|
-          format.html { redirect_to categories_url }
-      end
+    return unless user_signed_in?
+
+    respond_to do |format|
+      format.html { redirect_to categories_url }
     end
   end
 end
